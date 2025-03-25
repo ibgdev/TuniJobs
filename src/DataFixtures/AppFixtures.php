d4c81291c7a6cf67ec4d5c8aee19cf441de6a434
ibgdev <ibrahimghorbali605@gmail.com>
2025-03-26 00:18:29 +0100

    category Controller

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,17 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Category;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // for ($i = 0; $i < 10; $i++) {
+        //     $category = new Category();
+        //     $category->setNom("category" . $i);
+        //     $manager->persist($category);
+        // }
 
         $manager->flush();
     }
