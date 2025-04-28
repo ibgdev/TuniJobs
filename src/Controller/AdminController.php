b957070fa956b2c403abbdc081d00df0d4b1d562
ibgdev <ibrahimghorbali605@gmail.com>
2025-04-28 22:01:22 +0100

    crud styling

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +15,15 @@ final class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/admin/users', name:'admin.users')]
+    public function users_all(EntityManagerInterface $em): Response
+    {
+        $users = $em->getRepository(User::class)->findAll();
+        return $this->render('admin/users/index.html.twig', [
+            'users' => $users
         ]);
     }
 }
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -14,7 +14,6 @@ class CategoryType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('save', SubmitType::class)
         ;
     }
 
