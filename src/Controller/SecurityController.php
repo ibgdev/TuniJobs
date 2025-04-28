5c7ea79d09062ab7e10a2d3753f0f9e2f3527138
ibgdev <ibrahimghorbali605@gmail.com>
2025-04-28 20:07:35 +0100

    reset password done

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -38,11 +42,58 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+
     #[Route(path: '/my-account', name: 'app_account')]
-    public function my_account() : Response{
+    public function my_account(): Response
+    {
         $user = $this->getUser();
         return $this->render('security/myaccount.html.twig', [
             "user" => $user
         ]);
     }
+
+    #[Route(path: '/reset-password', name: 'password.reset', methods: ['POST'])]
+    public function reset_password(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $id = $request->request->get('id');
+        $user = $em->getRepository(User::class)->find($id);
+    
+        if (!$user) {
+            throw $this->createAccessDeniedException('You must be logged in to reset your password.');
+        }
+    
+        $currentPassword = $request->request->get('currentPassword');
+        $newPassword = $request->request->get('newPassword');
+        $confirmPassword = $request->request->get('confirmPassword');
+    
+        if (empty($currentPassword) || empty($newPassword) || empty($confirmPassword)) {
+            $this->addFlash('error', 'All fields are required.');
+            return $this->redirectToRoute('app_account');
+        }
+    
+        // Vérifier que le mot de passe actuel est correct
+        if (!$passwordHasher->isPasswordValid($user, $currentPassword)) {
+            $this->addFlash('error', 'Current password is incorrect.');
+            return $this->redirectToRoute('app_account');
+        }
+    
+        // Vérifier que les nouveaux mots de passe correspondent
+        if ($newPassword !== $confirmPassword) {
+            $this->addFlash('error', 'New passwords do not match.');
+            return $this->redirectToRoute('app_account');
+        }
+    
+        // Mettre à jour le mot de passe
+        $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+        $user->setPassword($hashedPassword);
+    
+        $em->persist($user);
+        $this->addFlash('message', 'Mot de pass mise à jour avec succès');
+        $em->flush();
+    
+        $this->addFlash('success', 'Your password has been updated successfully.');
+    
+        return $this->redirectToRoute('app_account');
+    }
+    
 }
